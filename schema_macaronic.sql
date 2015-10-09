fa1cbe30e0ec7d8fcdcd340b834d30fd2aba269f
arendu <adithya.r@gmail.com>
2015-10-09 12:12:20 -0400

    fixed typo

--- schema_macaronic.sql
+++ schema_macaronic.sql
@@ -1,25 +1,25 @@
 CREATE USER 'macaronic_admin'@'localhost' IDENTIFIED BY 'macaronic_password';
-create macaronicdb if not exists;
+CREATE DATABASE IF NOT EXISTS macaronicdb;
 GRANT ALL PRIVILEGES ON macaronicdb.* TO 'macaronic_admin'@'localhost';
 FLUSH PRIVILEGES;
 
-use macaronicdb;
+USE macaronicdb;
 
-drop table if exists  macaronicUsers;
+DROP TABLE IF EXISTS  macaronicUsers;
 
-create table macaronicUsers(
+CREATE TABLE macaronicUsers(
    id integer primary key auto_increment,
    username varchar(100) unique,
    password varchar(100)
 )engine=innodb charset utf8;
 
-drop table if exists macaronicRecords;
+DROP TABLE IF EXISTS macaronicRecords;
 
-create table macaronicRecords(
+CREATE TABLE macaronicRecords(
     id integer primary key auto_increment,
     username varchar(100) not null,
     rule text, 
     state_before text,
     state_after text,
-    created_at datetime not null default CURRENT_TIMESTAMP
+    CREATEd_at datetime not null default CURRENT_TIMESTAMP
 )engine=innodb charset utf8;
