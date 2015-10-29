c7815b524d25d8e109ce20df9a29bb2ac2b22340
arendu <adithya.r@gmail.com>
2015-10-29 16:26:01 -0400

    creates mturk tables

--- schema_macaronic.sql
+++ schema_macaronic.sql
@@ -1,31 +1,6 @@
-GRANT USAGE ON *.*  TO  'macaronic_admin'@'localhost';
-DROP USER 'macaronic_admin'@'localhost';
-CREATE USER 'macaronic_admin'@'localhost' IDENTIFIED BY '';
-
 CREATE DATABASE IF NOT EXISTS macaronicdb;
-GRANT ALL PRIVILEGES ON macaronicdb.* TO 'macaronic_admin'@'localhost';
-FLUSH PRIVILEGES;
 
 USE macaronicdb;
-
-DROP TABLE IF EXISTS  macaronicUsers;
-
-CREATE TABLE macaronicUsers(
-   id integer primary key auto_increment,
-   username varchar(100) unique,
-   password varchar(100)
-)engine=innodb charset utf8;
-
-DROP TABLE IF EXISTS macaronicRecords;
-
-CREATE TABLE macaronicRecords(
-    id integer primary key auto_increment,
-    username varchar(100) not null,
-    rule text, 
-    state_before text,
-    state_after text,
-    created_at fieldtype not null default CURRENT_TIMESTAMP
-)engine=innodb charset utf8;
 
 DROP TABLE IF EXISTS mturkUsers;
 
