37915143c1f68d9ad7f9543c4e5666d6dcf30581
arendu <adithya.r@gmail.com>
2015-12-01 22:13:37 -0500

    run de coe

--- python/run-coe-de.sh
+++ python/run-coe-de.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
-./coe-from-mt.py -i ../data/de/nachrichtenleicht/nachrichtenleicht.de.tok -o ../data/de/nachrichtenleicht/nachrichtenleicht.de.wa.untangled.true.aligned -e ../data/de/nachrichtenleicht/nachrichtenleicht.de.unsplit.edges.uniq.intermediate -p ../data/de/nachrichtenleicht/nachrichtenleicht.de.tok.dep.parsed  -s Artikel > ../data/de/nachrichtenleicht/nachrichtenleicht.de.js
+./coe-from-mt.py -i ../data/de/nachrichtenleicht/nachrichtenleicht.de.tok -o ../data/de/nachrichtenleicht/nachrichtenleicht.de.wa.untangled.true.aligned -e ../data/de/nachrichtenleicht/nachrichtenleicht.de.tok.edges.uniq.intermediate -p ../data/de/nachrichtenleicht/nachrichtenleicht.de.tok.dep.parsed  -s Artikel > ../data/de/nachrichtenleicht/nachrichtenleicht.de.js
 cp ../data/de/nachrichtenleicht/nachrichtenleicht.de.js ../stories/nachrichtenleicht.de.js
 
