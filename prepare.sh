207aa19e2b1a2499f521ea229e0f0515ffb2bc0b
arendu <adithya.r@gmail.com>
2019-11-19 16:13:00 -0500

    package lock used

--- prepare.sh
+++ prepare.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 PASSWORD (if running on www then use the password carl setup)" >&2
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 USERNAME PASSWORD for mysql (if running on www then use the password carl setup)" >&2
     exit 1
 fi
 PASSWORD=$1
-npm ci #uses package.lock.json
+PASSWORD=$2
+npm i package-lock.json #uses package.lock.json
 #npm install # uses package.json
-sed  "s/var config.*/var config = {host:'localhost', user:'macaronic_admin', password:'$PASSWORD', database:'macaronicdb'};/g" db_ref.js > db.js
+sed  "s/var config.*/var config = {host:'localhost', user:'$USERNAME', password:'$PASSWORD', database:'macaronicdb'};/g" db_ref.js > db.js
 MYSQLVER=$(mysql --help | grep Ver | head -1 | awk '{print $5}' | sed 's/,//g')
 sed "s/IDENTIFIED BY.*/IDENTIFIED BY '$PASSWORD';/g" schema_macaronic.sql > tmp 
 case "$MYSQLVER" in
